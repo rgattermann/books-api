e14dfbad62ae6167ac783a563f33710a2fde7ee5
Rodrigo Gattermann <rodrigo.gattermann@gmail.com>
2022-02-10 19:58:11 -0300

    Adding jest and users services tests

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,40 @@
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import { uuid } from 'uuidv4';
+
+class FakeUsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = this.users.find(u => u.id.toString() === id);
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find(u => u.email === email);
+
+    return user;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
+}
+
+export default FakeUsersRepository;
